6c06f5c8bae322232b25e408582c10c066e22b41
Mario <mario@paladinsec.co>
2022-04-30 00:21:49 +0300

    removed examples

--- contract-examples/contracts/INativeMinter.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity >=0.6.2;
-import "./IAllowList.sol";
-
-interface INativeMinter is IAllowList {
-  // Mint [amount] number of native coins and send to [addr]
-  function mintNativeCoin(address addr, uint256 amount) external;
-}
